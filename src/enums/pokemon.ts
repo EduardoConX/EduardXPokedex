027ab0bc81d89e96b1d2f61fe94ac1956e6f36d3
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-07-28 18:39:15 -0600

    Fifth generation added

--- src/enums/pokemon.ts
+++ src/enums/pokemon.ts
@@ -2,13 +2,15 @@ import firstGeneration from './pokemonGenerations/firstGeneration';
 import secondGeneration from './pokemonGenerations/secondGeneration';
 import thirdGeneration from './pokemonGenerations/thirdGeneration';
 import fourthGeneration from './pokemonGenerations/fourthGeneration';
+import fifthGeneration from './pokemonGenerations/fifthGeneration';
 
 const pokemon =
     [
         ...firstGeneration,
         ...secondGeneration,
         ...thirdGeneration,
-        ...fourthGeneration
+        ...fourthGeneration,
+        ...fifthGeneration,
     ];
 
 export default pokemon;
