51abdf80c65adbe284ce1bd2b5ef4a2a284643e1
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-07-28 18:06:40 -0600

    First generation added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import pokemon from "./enums/pokemon";
 </script>
 
 <template>
-  
+  <div class="grid grid-cols-4 gap-4">
+    <div class="flex flex-col"
+     v-for="i in 151" :key="i">
+      <img
+        :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`"
+        :alt="`${i}`"
+      />
+      <p class="text-2xl">{{ pokemon[i-1] }}</p>
+    </div>
+  </div>
 </template>
