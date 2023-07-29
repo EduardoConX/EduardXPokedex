62831ebdea24679492d5e282af62dde819828ff8
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-07-28 18:32:07 -0600

    Fourth generation added

--- src/enums/pokemon.ts
+++ src/enums/pokemon.ts
@@ -1,12 +1,14 @@
 import firstGeneration from './pokemonGenerations/firstGeneration';
 import secondGeneration from './pokemonGenerations/secondGeneration';
 import thirdGeneration from './pokemonGenerations/thirdGeneration';
+import fourthGeneration from './pokemonGenerations/fourthGeneration';
 
 const pokemon =
     [
         ...firstGeneration,
         ...secondGeneration,
-        ...thirdGeneration
+        ...thirdGeneration,
+        ...fourthGeneration
     ];
 
 export default pokemon;
