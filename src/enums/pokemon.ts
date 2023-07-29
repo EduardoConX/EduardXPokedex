824e4c0b8f083aa9f199fb936e98cf642c0c95e9
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-07-28 18:23:41 -0600

    Third generation added

--- src/enums/pokemon.ts
+++ src/enums/pokemon.ts
@@ -1,10 +1,12 @@
 import firstGeneration from './pokemonGenerations/firstGeneration';
 import secondGeneration from './pokemonGenerations/secondGeneration';
+import thirdGeneration from './pokemonGenerations/thirdGeneration';
 
 const pokemon =
     [
         ...firstGeneration,
         ...secondGeneration,
+        ...thirdGeneration
     ];
 
 export default pokemon;
