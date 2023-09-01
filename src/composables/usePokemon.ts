6d3a9ed2e234a120dc3d89a70ebe222034f3cdcf
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-09-01 15:10:24 -0600

    Get Pokemon Data

---
+++ src/composables/usePokemon.ts
@@ -0,0 +1,57 @@
+import { ref, watch } from "vue";
+import { useQuery } from "@tanstack/vue-query";
+import { child, get, ref as firebaseRef } from "firebase/database";
+import { PokemonInfo } from "../interfaces/PokemonInfo";
+import { database } from "./../database/config";
+
+const dbRef = firebaseRef(database);
+
+const getPokemon = async (id: string) => {
+  let data = [];
+  try {
+    const snapshot = await get(child(dbRef, id));
+    if (snapshot.exists()) {
+      data = snapshot.val();
+    }
+    return snapshot.val();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return data;
+};
+
+const usePokemon = (id: string) => {
+  const pokemon = ref<PokemonInfo>({
+    pokemon: "",
+    occurrences: "",
+    pokedex: false,
+  });
+
+  const { isLoading, data, isError } = useQuery(
+    ["pokemon", id],
+    () => getPokemon(id),
+    {
+      retry: false,
+      onError(error: any) {
+        console.error(error);
+      },
+    }
+  ); 
+
+  watch(
+    data,
+    () => {
+      if (data.value) pokemon.value = data.value;
+    },
+    { immediate: true }
+  );
+
+  return {
+    pokemon,
+    isLoading,
+    isError,
+  };
+};
+
+export default usePokemon;
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import pokemon from "../enums/pokemon";
+
+import usePokemon from "../composables/usePokemon";
 
 const { params } = useRoute();
-const id = +params.id;
+const id = params.id;
+const { isLoading, pokemon } = usePokemon(id as string);
 </script>
 
 <template>
-  <h1 class="text-3xl md:text-6xl font-bold mb-4">{{ pokemon[id] }}</h1>
-  <img
-    class="mx-auto"
-    height="500"
-    width="500"
-    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-      id + 1
-    }.png`"
-    :alt="`${id + 1}`"
-  />
+  <p v-if="isLoading">Cargando...</p>
+  <div v-else>
+    <h1 class="text-3xl md:text-6xl font-bold mb-4">{{ pokemon?.pokemon }}</h1>
+    <img
+      class="mx-auto"
+      height="500"
+      width="500"
+      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+        +id + 1
+      }.png`"
+      :alt="`${+id + 1}`"
+    />
+    <p class="text-2xl">Disponibles: {{ pokemon?.occurrences }}</p>
+    <p class="text-2xl">
+      ¿En PC de EduardX? {{ pokemon?.pokedex ? "Si" : "No" }}
+    </p>
+  </div>
 </template>
