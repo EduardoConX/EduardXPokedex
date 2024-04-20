6517bb2219078a9c3c5e999272b60a988e72a87d
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2024-04-19 19:11:00 -0600

    Show each ocurrence of the Pokemon

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,15 +2,19 @@
 import { useRoute } from "vue-router";
 
 import usePokemon from "../composables/usePokemon";
+import usePositions from "../composables/usePositions";
 
 const { params } = useRoute();
 const id = params.id;
 const { isLoading, pokemon } = usePokemon(id as string);
-
+const { isLoading: isLoadingPositions, positions } = usePositions();
+const ocurrences = positions.value.filter(
+  (position) => position.id == Number(id) + 1
+);
 </script>
 
 <template>
-  <p v-if="isLoading">Cargando...</p>
+  <p v-if="isLoading || isLoadingPositions">Cargando...</p>
   <div v-else>
     <h1 class="text-3xl md:text-6xl font-bold mb-4">{{ pokemon?.pokemon }}</h1>
     <img
@@ -23,10 +27,15 @@ const { isLoading, pokemon } = usePokemon(id as string);
       :alt="`${+id + 1}`"
     />
     <p class="text-2xl">
-      Disponibles: {{ pokemon?.occurrences }}
-    </p>
-    <p class="text-2xl">
       ¿En PC de EduardX? {{ pokemon?.pokedex ? "Si" : "No" }}
     </p>
+    <div v-if="ocurrences.length">
+      <p class="text-2xl">Disponibles:</p>
+      <ul>
+        <li v-for="ocurrence in ocurrences" class="text-xl">
+          - Nivel: {{ ocurrence.level }} Posición: {{ ocurrence.position }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
